5b02206e0ab8605a7f7a07badade17ad6bcf923e
Erik Tollerud <erik.tollerud@gmail.com>
2022-11-03 00:10:31 -0400

    working blink on analog

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,12 @@ use bsp::pac;
 use circuit_playground_express as bsp;
 
 use bsp::entry;
+use hal::adc::Adc;
 use hal::clock::GenericClockController;
 use hal::delay::Delay;
 use hal::prelude::*;
 use hal::timer::TimerCounter;
+
 use pac::{CorePeripherals, Peripherals};
 
 use smart_leds::{
@@ -51,17 +53,38 @@ fn main() -> ! {
     let neopixel_pin: bsp::NeoPixel = pins.d8.into();
     let mut neopixel = ws2812::Ws2812::new(timer, neopixel_pin);
 
-    // Loop through all of the available hue values (colors) to make a
-    // rainbow effect from the onboard neopixel
+    
+    let mut adc = Adc::adc(peripherals.ADC, &mut peripherals.PM, &mut clocks);
+    let mut a1 : hal::gpio::v2::Pin<hal::gpio::v2::pin::PA05, hal::gpio::v2::Alternate<hal::gpio::v2::B>> = pins.a1.into();
+    let mut a2 : hal::gpio::v2::Pin<hal::gpio::v2::pin::PA06, hal::gpio::v2::Alternate<hal::gpio::v2::B>> = pins.a2.into();
+
     loop {
-        for j in 0..255u8 {
-            let colors = [hsv2rgb(Hsv {
-                hue: j,
-                sat: 255,
-                val: 2,
-            }); 10];
-            neopixel.write(colors.iter().cloned()).unwrap();
-            delay.delay_ms(5u8);
-        }
+        // to span the full hue range, data / 16, but this maps to 0-240 deg i.e.red to blue
+
+        let data1: u16 = adc.read(&mut a1).unwrap();
+        let hue1: u8 = (data1 *15 / 360).try_into().unwrap();  
+        let rgb1 = hsv2rgb(Hsv {
+            hue: hue1,
+            sat: 255,
+            val: 2,
+        });
+
+        let data2: u16 = adc.read(&mut a2).unwrap();
+        let hue2: u8 = (data2 *15 / 360).try_into().unwrap();  
+        let rgb2= hsv2rgb(Hsv {
+            hue: hue2,
+            sat: 255,
+            val: 2,
+        });
+        
+        
+        let mut colors = [rgb1; 10];
+        for i in 0..3 { colors[i] = rgb2; }
+        for i in 8..10 { colors[i] = rgb2; }
+
+        neopixel.write(colors.iter().cloned()).unwrap();
+        delay.delay_ms(100u16);
     }
 }
+
+
